3ec432e6350bbaa1a32caa353ac30bd8bb20a86b
Mahambet <mahambet.007@mail.ru>
2020-03-14 00:31:25 +0600

    Created test

--- credit/models.py
+++ credit/models.py
@@ -1,24 +1,19 @@
 from django.db import models
-from django.utils.timezone import now
 
 
 class Person(models.Model):
-    First_name = models.CharField(max_length=25, verbose_name="Имя")
-    Last_name = models.CharField(max_length=25, verbose_name="Фамилия")
+    first_name = models.CharField(max_length=25, verbose_name="Имя")
+    last_name = models.CharField(max_length=25, verbose_name="Фамилия")
     iin = models.CharField(max_length=12, verbose_name="ИИН")
-    Phone = models.CharField(max_length=11, verbose_name="Телефон")
+    phone = models.CharField(max_length=11, verbose_name="Телефон")
 
 
-class CreditStatus(models.Model):
+class Credit(models.Model):
     CHOICES = (
         ('CREATED', 'created'),
         ('ACTIVE', 'active'),
         ('PAID', 'paid'),
     )
     status = models.CharField(max_length=150, verbose_name="статус", choices=CHOICES)
-    credit = models.ForeignKey(Person, related_name='statuses', on_delete=models.CASCADE)
-
-
-
-
-# Create your models here.
+    cash = models.DecimalField(max_digits=5, decimal_places=3, verbose_name="Денги")
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
--- credit/tests.py
+++ credit/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
-def
-# Create your tests here.
+import pytest
+from . import models
+
+
+def test_credit(db):
+    data = {
+        'first_name': 'Максим',
+        'last_name': 'Балабанов',
+        'iin': '911106451061',
+        'phone': '77778464774'
+    }
+    person = models.Person.objects.create(**data)
+    data = {
+        'cash': 30,
+        'status': 'active',
+        'person': person
+    }
+    credit = models.Credit.objects.create(**data)
+    assert isinstance(credit, models.Credit)
